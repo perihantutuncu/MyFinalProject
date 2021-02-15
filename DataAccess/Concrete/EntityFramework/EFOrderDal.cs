7e3e548e5af393b36ba3e04535229ee33844022b
Perihan TÜTÜNCÜ <perihantutuncu@hotmail.com>
2021-02-15 11:08:06 +0300

    feat:categoryOperationsAdded feat:coreProjectAdded feat:dtoOperationsAdded

--- DataAccess/Concrete/EntityFramework/EFOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EFOrderDal.cs
@@ -3,12 +3,11 @@ using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EFCategoryDal : EFEntityRepositoryBase<Category, NortwindContext>, ICategoryDal
+    public class EFOrderDal : EFEntityRepositoryBase<Order, NortwindContext>, IOrderDal
     {
 
     }
